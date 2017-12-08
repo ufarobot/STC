d8466a931aab2c44b20999edb21f2f5584f8cbe7
ufarobot <ufarobot@gmail.com>
2017-12-08 18:10:28 +0300

    Day5Task1 Реализация EmployeeManager

--- src/Serialization/EmployeeManager.java
+++ src/Serialization/EmployeeManager.java
@@ -5,32 +5,49 @@ import java.util.ArrayList;
 
 public class EmployeeManager {
     private final String fileName;
-    private ArrayList<Employee> employees;
 
     public EmployeeManager(String fileName) {
         this.fileName = fileName;
-        employees = new ArrayList<>();
+
+        // creates new file, if doesn't exists or delete old data from existing file
+        prepareFile(fileName);
     }
 
-    public static void main(String[] args) {
-        EmployeeManager manager = new EmployeeManager("manager1.txt");
-        Employee employee = new Employee("Ivan", 21, 1000, "manager");
-        manager.save(employee);
-        manager.displayEmployee(manager.getByName("Ivan"));
-        for (Employee emp : manager.getByJob("manager"))
-            manager.displayEmployee(emp);
+    private void prepareFile(String fileName) {
+        try {
+            new PrintWriter(fileName).close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public Iterable<Employee> getByJob(String job) {
-        ArrayList<Employee> resultList = new ArrayList<>();
+    private Employee delete(String name) {
+        ArrayList<Employee> employees = getEmployees();
+        for (Employee employee : employees)
+            if (employee.name.equals(name)) {
+                employees.remove(employee);
+                writeEmployeesToFile(employees);
+                return employee;
+            }
+        return null;
+    }
+
+    private ArrayList<Employee> getEmployees() {
         try {
             ObjectInputStream file = new ObjectInputStream(new FileInputStream(fileName));
-            employees = (ArrayList<Employee>) file.readObject();
+            return  (ArrayList<Employee>) file.readObject();
+        } catch (EOFException e){ // If file is empty, return empty list
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return new ArrayList<>();
+    }
+
+    public Iterable<Employee> getByJob(String job) {
+        ArrayList<Employee> employees = getEmployees();
+        ArrayList<Employee> resultList = new ArrayList<>();
         for (Employee employee : employees)
-            if (employee.name.equals(job))
+            if (employee.job.equals(job))
                 resultList.add(employee);
         return resultList;
     }
@@ -44,12 +61,7 @@ public class EmployeeManager {
     }
 
     public Employee getByName(String name) {
-        try {
-            ObjectInputStream file = new ObjectInputStream(new FileInputStream(fileName));
-            employees = (ArrayList<Employee>) file.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        ArrayList<Employee> employees = getEmployees();
         for (Employee employee : employees)
             if (employee.name.equals(name))
                 return employee;
@@ -57,17 +69,43 @@ public class EmployeeManager {
     }
 
     public void save(Employee employee) {
-        add(employee);
+        if (employee == null) throw new IllegalArgumentException();
+        ArrayList<Employee> employees = getEmployees();
+        employees.add(employee);
+        writeEmployeesToFile(employees);
+    }
+
+    private void writeEmployeesToFile(ArrayList<Employee> employees) {
         try {
             ObjectOutputStream file = new ObjectOutputStream(new FileOutputStream(fileName));
-            file.writeObject(employee);
+            file.writeObject(employees);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void add(Employee employee) {
-        if (employee == null) throw new IllegalArgumentException();
-        employees.add(employee);
+    // Unit testing
+    public static void main(String[] args) {
+        EmployeeManager manager = new EmployeeManager("manager1.txt");
+
+        Employee employee1 = new Employee("Ivan", 21, 1000, "manager");
+        Employee employee2 = new Employee("Petr", 22, 2000, "manager");
+        Employee employee3 = new Employee("John", 23, 1000, "developer");
+
+        manager.save(employee1);
+        manager.save(employee2);
+        manager.save(employee3);
+
+        manager.displayEmployee(manager.getByName("Petr"));
+
+        System.out.println("managers------------------------");
+        for (Employee emp : manager.getByJob("manager"))
+            manager.displayEmployee(emp);
+
+        manager.delete("Petr");
+        manager.displayEmployee(manager.getByName("Petr"));
+        System.out.println("managers ------------------------");
+        for (Employee emp : manager.getByJob("manager"))
+            manager.displayEmployee(emp);
     }
 }
